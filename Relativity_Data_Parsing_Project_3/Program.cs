0d8e1d90e072ae4c0a6b524e36e540682e4ce3bf
joshp123 <joshp123@users.noreply.github.com>
2013-01-24 20:49:12 +0000

    Initial commit; file parsing code complete
    woo regex *kills self irl*

---
+++ Relativity_Data_Parsing_Project_3/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+// used for parsing the file
+
+namespace Relativity_Data_Parsing_Project_3
+{
+    class Program
+    {
+        /// <summary>
+        /// Event structure
+        /// </summary>
+        /// <remarks>
+        /// Contains fields for times and energy
+        /// </remarks>
+        public struct Event
+        {
+            public double time_1;
+            public double time_2;
+            public double energy;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\Josh\Downloads\p3data7.dat";
+
+            List<Event> Events = ParseFile(filepath);
+
+            // take some quick data on the events
+
+
+        }
+
+
+        static List<Event> ParseFile(string filepath)
+        {
+            System.IO.StreamReader inputFile = null;
+
+            var Events = new List<Event>();
+            // List of events
+
+            try
+            {
+                inputFile = new System.IO.StreamReader(filepath);
+            }
+
+            catch (System.IO.FileNotFoundException ex)
+            {
+                // Put the more specific exception first.
+                 // TODO: implement exceptions fully. Prompt user for filepath (default to wherever i'm saving them), and if it doesn't exist return and reprompt
+                System.Console.WriteLine(ex.ToString());
+            }
+
+            string line;
+            
+            // read the full file line by line
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                // ignore header lines (lines beginning with '#' or blank lines
+                // for some reason it has the line as "" and it's not equal to null and doesn't terminate so that's why there's the length thing idfk
+                if ((line.Length == 0) || (line[0] == '#'))
+                {
+                    continue;
+                }
+                
+                var thisevent = new Event();
+                // declare variable for the event we're reading off this line
+                
+                // use a regular expression to extract the 3 variables
+                try
+                {
+                    thisevent.time_1 = Convert.ToDouble(Regex.Match(line, @"(\d*.?\d*)(?=\st2\s=)").ToString());
+                    thisevent.time_2 = Convert.ToDouble(Regex.Match(line, @"(\d*.?\d*)(?=\sE\s=)").ToString());
+                    thisevent.energy = Convert.ToDouble(Regex.Match(line, @"(-?\d*.?\d*)$").ToString());
+
+                    // TODO: modify the regex to handle malformed files
+
+                    // TODO: (optional) figure out how long the regex is taking and maybe use RegexOptions.Compiled isntead: http://www.dotnetperls.com/regexoptions-compiled
+                }
+                catch (Exception)
+                {
+                    // throw exception if values don't cast to floats
+                    // write the errant values and line number here 
+                    // Error on line xxx:
+                    // t1 = xxx t2 = e = 
+                    // <line>
+                    throw;
+                }
+
+                Events.Add(thisevent);
+                // append this event
+            }
+
+            return Events;
+            // should return a fully populated list
+        }
+    }
+}
